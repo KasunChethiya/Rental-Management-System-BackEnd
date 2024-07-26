2b8c69a49f42ec852950aa85d9e0e9bb8c1cf6f2
KasunChethiya <kcrathnamalala@gmail.com>
2024-07-26 14:28:40 +0530

    created RentalController and it's mappings

--- src/main/java/edu/icet/crm/controller/RentalController.java
+++ src/main/java/edu/icet/crm/controller/RentalController.java
@@ -1,10 +1,15 @@
 package edu.icet.crm.controller;
 
+import edu.icet.crm.dto.Customer;
 import edu.icet.crm.dto.Rental;
+import edu.icet.crm.entity.CustomerEntity;
+import edu.icet.crm.entity.RentalEntity;
 import edu.icet.crm.service.RentalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/rental-controller")
@@ -15,7 +20,31 @@ public class RentalController {
     @PostMapping("add-rental")
     @ResponseStatus(HttpStatus.CREATED)
     public void addRental(@RequestBody Rental rental){
+        rentalService.addRental(rental);
 
+    }
+    @GetMapping("get-all-rental")
+    public List<RentalEntity> getAll(){
+        return rentalService.getAll();
+    }
+    @DeleteMapping("/delete-rental/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String deleteRental(@PathVariable Long id){
+        rentalService.deleteRentalById(id);
+        return "Deleted";
+    }
+    @PostMapping("/update-rental")
+    public void updateRental(@RequestBody Rental rental){
+        rentalService.updateRental(rental);
+    }
+    @GetMapping("/find-by-rental-id/{id}")
+    public Customer findById(@PathVariable Long id){
+        return rentalService.findById(id);
+    }
+
+    @GetMapping("find-by-name/{name}")
+    public Customer findByName(@PathVariable String name){
+        return rentalService.findByName(name);
     }
 
 }
--- src/main/java/edu/icet/crm/service/RentalService.java
+++ src/main/java/edu/icet/crm/service/RentalService.java
@@ -1,4 +1,21 @@
 package edu.icet.crm.service;
 
+import edu.icet.crm.dto.Customer;
+import edu.icet.crm.dto.Rental;
+import edu.icet.crm.entity.RentalEntity;
+
+import java.util.List;
+
 public interface RentalService {
+    void addRental(Rental rental);
+
+    List<RentalEntity> getAll();
+
+    void deleteRentalById(Long id);
+
+    void updateRental(Rental rental);
+
+    Customer findById(Long id);
+
+    Customer findByName(String name);
 }
